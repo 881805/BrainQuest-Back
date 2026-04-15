e6f7fd265e1365f71489d881ebcd0b01b1d7cf14
SebbingtonTaken <128566685+SebbingtonTaken@users.noreply.github.com>
2026-04-15 17:17:57 -0600

    afads
    dasddsa

--- src/main/java/com/project/demo/scheduler/MissionScheduler.java
+++ src/main/java/com/project/demo/scheduler/MissionScheduler.java
@@ -23,7 +23,7 @@ public class MissionScheduler {
 
     // This will run every 5 minutes (300,000 milliseconds)
     @Scheduled(fixedRate = 60000, initialDelay = 10000) // espera 10s antes de la primera ejecución
-    public void updateExpiredMissions() { 
+    public void updateExpiredMissions() {
         List<Mission> expiredMissions = missionRepository.findByEndDateBeforeAndIsActiveTrue(LocalDate.now());
 
         for (Mission mission : expiredMissions) {
